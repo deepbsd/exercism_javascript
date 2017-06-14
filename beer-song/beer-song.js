76b35ed96b46ed80b32e8a54111d059050bdfc6c
David S. Jackson <dsj@hackmac.local>
2017-06-14 12:50:36 -0400

    finished beer song

--- beer-song/beer-song.js
+++ beer-song/beer-song.js
@@ -1,44 +1,48 @@
-var BeerSong = function() {
-}
+var BeerSong = function() {}
 
-BeerSong.prototype.verse = function(n1, n2=n1) {
-  var start = n1;
-  var end = n2;
-  var plural = 'bottles';
-  var singular = 'bottle';
-  var bottles1 = plural;
-  var bottles2 = plural;
-  var one = 'one';
-  var output = '';
-  var count;
+BeerSong.prototype.verse = function (num) {
+  if (typeof num !== 'number') throw new Error('BeerSong.verse expects a verse number');
+  var num1 = num, num2 = num1; 
+  var num3 = num-1;
+  var str1 = 'Take one down and pass it around';
+  var str2 = 'Go to the store and buy some more';
+  var str3 = 'Take it down and pass it around';
+  var bottles1 = 'bottles', bottles2 = bottles1;
+  var action = str1;
+
+  if (num === 2)  bottles2 = 'bottle';
   
 
-  for (var num=start; num>=end; num--) {
-    count = num-1; 
-    if (num === 2) { bottles1 = plural; bottles2 = singular; }
-    if (num === 1) { 
-      bottles1 = singular; 
-      bottles2 = plural; 
-      one = 'it'; 
-      count = 'no more'; 
-    }
-    if (num === 0) {
-      num = 'No more';
-      bottles1 = plural;
-      str1 = 'Take one down and pass it around,';
-      str2 = 'Go to the store and buy some more,';
-      count = 99;
-      bottles2 = plural;
-      template.replace(str1, str2);
-    }
-    var template = `${num} ${bottles1} of beer on the wall, ${num} ${bottles1} of beer.\nTake ${one} down and pass it around, ${count} ${bottles2} of beer on the wall.\n`
-    
-    output += template; 
+  if (num === 1) {
+    bottles1 = 'bottle'; bottles2 = 'bottles';
+    num3 = 'no more';
+    action = str3;
+  }
+
+  if (num === 0) {
+    num1 = 'No more';
+    num2 = 'no more';
+    num3 = 99;
+    action = str2;
+  }
+
+  var template = `${num1} ${bottles1} of beer on the wall, ${num2} ${bottles1} of beer.\n${action}, ${num3} ${bottles2} of beer on the wall.\n`;
+
+
+  return template;
+}
+
+BeerSong.prototype.sing = function (start, end=0) {
+  if (typeof start !== 'number') throw new Error('BeerSong.sing expects a starting verse number');
+  
+  var output = '';
+
+  for (var n=start; n>=end; n--){
+    if (n===end) output += this.verse(n);
+    else  output += this.verse(n)+'\n';      
   }
 
   return output;
 }
-
-
 
 module.exports = BeerSong;
---
+++ phone-number/phone-number.js
@@ -0,0 +1,12 @@
+module.exports = PhoneNumber(num) = {
+	
+	number: function() {
+
+	}
+	areaCode: function() {
+
+	}
+	toString: function() {
+
+	}
+}
