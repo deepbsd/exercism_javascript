995344c15eacee0ad4246dde56602a5bd78b0935
David S. Jackson <deepbsd@yahoo.com>
2017-06-17 03:17:37 -0400

    added exercises

--- phone-number/phone-number.js
+++ phone-number/phone-number.js
@@ -1,12 +1,22 @@
-module.exports = PhoneNumber(num) = {
+function PhoneNumber(num) {
 	
-	number: function() {
-
+	this.number = function() {
+		if ((num.match(/\d+/g).join('').length > 11) || 
+			(num.match(/\d+/g).join('').length < 10) || 
+			((num.match(/\d+/g).join('').length === 11) && 
+				(num.match(/^\d/g).join('') !== '1'))) {
+		 	return '0000000000';
+		}
+		return num.match(/\d+/g).join('').slice(-10, num.length)
 	}
-	areaCode: function() {
-
+	this.areaCode = function() {
+		return this.number().slice(0,3);
 	}
-	toString: function() {
-
+	this.toString = function() {
+		return '(' + this.number().slice(0,3) + ') ' 
+		+ this.number().slice(3,6) + '-' + this.number().slice(6, 10);
 	}
 }
+
+
+module.exports = PhoneNumber;
