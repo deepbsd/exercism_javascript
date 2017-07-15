42b339918a4a3078ab599157947cb44e94767a2d
David S. Jackson <deepbsd@yahoo.com>
2017-07-15 15:27:10 -0400

    got food-chain working using a different approach

--- food-chain/food-chain.js
+++ food-chain/food-chain.js
@@ -4,7 +4,7 @@ function FoodChain() {
   // I think I'll change it to be more functional and modular.  But it's not done yet!
   // I was thinking of something like:
 
-  var repeat = 'I know an old lady who swalled a ';
+  var repeat = 'I know an old lady who swallowed a ';
 
 
   var theSong = {
@@ -13,7 +13,7 @@ function FoodChain() {
               versenum: 1, 
               food: 'fly',
               verse:  function() {
-                return repeat+this.food+'.'+'\nI don\'t know why she swallwed the fly. Perhaps she\'ll die.\n';
+                return repeat+this.food+'.'+'\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
               }
               
         },
@@ -22,7 +22,7 @@ function FoodChain() {
               food: 'spider',
               verse: function() {
                 var why = bridge(this.versenum);
-                return repeat+this.food+'.'+'\nIt wriggled and jiggled and tickled inside her '+why;
+                return repeat+this.food+'.'+'\nIt wriggled and jiggled and tickled inside her.\n'+why;
               }
         },
         3: {
@@ -80,28 +80,17 @@ function FoodChain() {
         var current_verse_num = verse;
         var bridge_string = '';
         for (var v = current_verse_num; v >= 2; v--) {
-          bridge_string += ' She swallowed the '+theSong[v].food+' to catch the '+theSong[v-1].food+'.' ;
-          if (v === 3) { bridge_string += " that wriggled and jiggled and tickled inside her.\n"; }
-          if (v === 2) { bridge_string += " I don't know why she swalled the fly. Perhaps she\'ll die.\n"; }
+          bridge_string += 'She swallowed the '+theSong[v].food+' to catch the '+theSong[v-1].food+'.\n' ;
+          bridge_string = bridge_string.replace('catch the spider.\n', 'catch the spider that wriggled and jiggled and tickled inside her.\n');
+          if (v === 2) { bridge_string += "I don't know why she swallowed the fly. Perhaps she\'ll die.\n"; }
         }
         return bridge_string;
   }
 
-  // Here's where the working part starts.  I'll remove $song once I get the other parts working...
-
-  var song = [
-    "I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n",
-    "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n",
-    "I know an old lady who swallowed a bird.\nHow absurd to swallow a bird!\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n",
-    "I know an old lady who swallowed a cat.\nImagine that, to swallow a cat!\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n",
-    "I know an old lady who swallowed a dog.\nWhat a hog, to swallow a dog!\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n",
-    "I know an old lady who swallowed a goat.\nJust opened her throat and swallowed a goat!\nShe swallowed the goat to catch the dog.\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n",
-    "I know an old lady who swallowed a cow.\nI don\'t know how she swallowed a cow!\nShe swallowed the cow to catch the goat.\nShe swallowed the goat to catch the dog.\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n",
-    "I know an old lady who swallowed a horse.\nShe\'s dead, of course!\n"
-  ]
 
   function verse(number) {
-        return song[number-1];
+        //return song[number-1];
+        return theSong[number].verse();
   }
 
   function verses(start, end){
