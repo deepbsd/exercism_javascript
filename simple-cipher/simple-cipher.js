f9dc65c78563bad8c8c5870c279b4316e6bc1a76
David S. Jackson <deepbsd@yahoo.com>
2017-09-24 22:30:57 -0400

    still working on solution

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,36 +1,45 @@
-(function() {
-	var Cipher = {
+module.exports = function(userKey=false) {
 
-		constructor: function(key){
-			this.key = key || this.generateKey();
-			this.plain = 'abcdefghijklmnopqrstuvwxyz'
-			this.ciph = app.plain.substr(this.key.length, plain.length+1) + plain.substr(0, this.key.length);
-		},
-		encode: function(plaintext){
-			var output = '';
-			for (var c = 0; c < plaintext.length; c++){
-				output += this.ciph.charAt(this.plain.indexOf(plaintext[c]));
-			}
-			return output;
-		},
-		decode: function(securetext){
-			var output = '';
-			for (var c = 0; c < securetext.length; c++){
-				output += this.plain.charAt(this.ciph.indexOf(plaintext[c]));
-			}
-			return output;
-		},
-		generateKey: function(){
-			var output = '';
-			for (var n=0; n<=100; n++){
-				var x = Math.floor(Math.random()*26)
-				output += this.plain.slice(x, x+1);
-			}
-			return output;
-		},
-		plain: 'abcdefghijklmnopqrstuvwxyz'
-	};
+	var alpha = 'abcdefghijklmnopqrstuvwxyz';
+  this.key = userKey || generateKey(alpha);
 
-})()
 
-module.exports = Cipher;
+	// if ( !(/^[a..z]+$/.test(this.key)) ) {
+	// 	throw new Error('Bad key');
+	// }
+
+
+	function generateKey(alpha){
+		var output = '';
+		for (var n=0; n<=100; n++){
+			var x = Math.floor(Math.random()*26)
+			output += alpha[x];
+		}
+		console.log('output: ',output)
+		return output;
+	}
+
+	function encode(plaintext){
+		var result = [];
+		var count = 0;
+		plaintext.split('').forEach(function(char){
+			var n = alpha.indexOf(char) + alpha.indexOf(this.key[count]);
+			if (n<0) { n += 26 }
+			result = result.push(alpha[n])
+			count++;
+		})
+		return result.join('');
+	}
+
+	function decode(ciphertext){
+		var result = [];
+		var count = 0;
+		ciphertext.split('').forEach(function(char){
+			var n = alpha.indexOf(char) - alpha.indexOf(this.key[count]);
+			if (n<0) { n += 26 }
+			result.push(alpha[n]);
+		})
+		return result.join('');
+	}
+
+};
