0c30112c6812eaa5c259d2bbd45c70b5bc904e6e
David S. Jackson <deepbsd@yahoo.com>
2017-09-28 20:08:02 -0400

    added matrix exercise

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -2,30 +2,45 @@ function Matrix(string){
   this.string = string;
   this.numrows = (this.string.match(/\n/g) || []).length+1;
   this.rows = createRows(this.string);
-  //this.columns = createColumns(this.string);
+  this.columns = createColumns(this.string);
 
   function createRows(string){
     var horiz = [];
+    var newlines = [];
     var numrows = (string.match(/\n/g) || []).length+1;
-    var numcols = string.slice(0,string.indexOf('\n')+1).replace(/\s/g,'').split('').length;
-    var slicestart = 0; var sliceend = numcols+1;
-    for (var n=0; n < numrows; n++){
-      horiz[n] = string.slice(slicestart, sliceend).replace(/\s/g,'').split(' ')
-      slicestart = sliceend+1;
-      sliceend = slicestart + numcols + 1;
+    for (var c=0; c < string.length; c++){
+      if (string.charAt(c) === '\n'){
+        newlines.push(c);
+      }
     }
-    console.log('numrows: ',numrows)
+    var slicestart = 0;
+    for (var n=0; n < numrows; n++){
+      sliceend = newlines[n]
+      horiz[n] = string.slice(slicestart, sliceend).split(' ');
+      horiz[n].forEach(function(val,index){
+        horiz[n][index] = parseInt(val);
+      })
+      slicestart = sliceend+1;
+    }
     return horiz;
   }
 
-  // function createColumns(string){
-  //   var vert = [];
-  //   for (var n=0; n < this.numrows; n++){
-  //     vert[n] = this.rows[n]
-  //   }
-  //   return vert;
-  // }
+  function createColumns(string){
+    var vert = [];
+    var rows = createRows(string);
+    var numrows = rows.length;
+    var numcols = rows[0].length;
 
+    for (var row=0; row < numcols; row++){
+      var temparr = [];
+      for (var col=0; col < numrows; col++){
+        temparr.push(rows[col][row]);
+
+      }
+      vert.push(temparr);
+    }
+    return vert;
+  }
 }
 
 module.exports = Matrix;
