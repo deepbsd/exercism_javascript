bb550153996c533dc95453aa6731de283976addb
David S. Jackson <deepbsd@yahoo.com>
2018-12-08 16:43:14 -0500

    still working ...

--- secret-handshake/secret-handshake.js
+++ secret-handshake/secret-handshake.js
@@ -8,28 +8,40 @@ function SecretHandshake (num) {
 
 SecretHandshake.prototype.commands = function () {
   let arr = [ "wink", "double blink", "close your eyes", "jump" ];
+
+
+  // arr will get consumed.  replenish with replacment
   const replacementArr = [ "wink", "double blink", "close your eyes", "jump" ];
+
+
+  // use these variables
   let count = 0;
   let num = this.number;
   let handshake = [];
 
 
+  // Keep shifting bits until we hit zero
   while (num != 0) {
       num = num >> 1;
-      //handshake.push(arr.shift());
+      
+      // replenish arr as needed
       if (count>3){ 
           arr = replacementArr;
       } 
-      //handshake = (count<3) ? [ arr.shift() ] : handshake.push(arr.pop()) ;
-      handshake = (count<4) ? [ arr.shift() ] : [ ...handshake, arr.shift() ]
-      console.log(`num: ${this.number}  num>>1: ${num>>1} count: ${count}  handshake: ${handshake} ==>ARR: [${arr}]`)
+      
+      // Just one value in the answer until we get to '8', then start appending
+      handshake = (count<4) ? [ arr.shift() ] : [ ...handshake, arr.shift() ];
+      
+      // for debugging --- check the values
+      console.log(`###num: ${this.number}  num>>1: ${num>>1} count: ${count}  handshake: ${handshake} ==>ARR: [${arr}]`);
+
+      // increment the count always
       count++;
-      //count = (count>=4) ? 0 : count++ ;
-      //count = (count<3) ? count++ : 0 ;
-      //console.log("handshake: ", handshake )
   }
 
    
+  // If a number greater than '10000' in binary is passed in, reverse the order
+  if (this.number >= 16){ handshake = handshake.reverse(); }
   return handshake;
 
 
