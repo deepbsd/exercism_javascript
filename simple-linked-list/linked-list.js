02f89b20e1e4ead6ba5972e700a0cb19a0e8d3c2
David S. Jackson <deepbsd@yahoo.com>
2018-12-09 12:17:02 -0500

    finished secret-handshake, just starting simple-linked-list

---
+++ simple-linked-list/linked-list.js
@@ -0,0 +1,44 @@
+// Implement a Singly Linked List
+class List {
+    constructor(){
+
+    }
+
+    pop(){
+
+    }
+
+    push(){
+
+    }
+
+    reverse(){
+
+    }
+
+    shift(){
+
+    }
+
+    toArray(){
+
+    }
+
+    unshift(){
+
+    }
+
+
+}
+
+List.fromArray = function(arr){
+
+}
+
+class Element {
+
+
+}
+
+
+module.exports = { List, Element }
