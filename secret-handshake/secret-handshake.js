af0a3d00f43ff077e2b1605ed49fcfdaf82155ba
David S. Jackson <deepbsd@yahoo.com>
2018-12-08 23:15:30 -0500

    so closegit add .git add .

--- secret-handshake/secret-handshake.js
+++ secret-handshake/secret-handshake.js
@@ -7,8 +7,6 @@ function SecretHandshake (num) {
 
 SecretHandshake.prototype.commands = function () {
   let arr = [ "wink", "double blink", "close your eyes", "jump" ];
-
-  // variables
   let num = this.number;
   let handshake = [];
   let binaryStr = num.toString(2);
@@ -19,23 +17,18 @@ SecretHandshake.prototype.commands = function () {
   // start counting significant bits as if from right, but actually from left
   for (let c=0; c<reverseStr.length; c++){
       
-      if (c>3){ 
-          handshake.pop();
-      }
+      // no duplicate commands
+      if (c>3){ handshake.pop(); }
 
       if (reverseStr[c] === '1'){ 
           if (arr[c]){
               handshake.unshift(arr[c])
-          }else{
-              // start over from left of array
-              handshake.unshift(arr[c%4])
-          }
+          }else{ handshake.unshift(arr[c%4]); }
       }
   }
 
   // reverse order of operations if more than '10000'
   if (this.number > 16){ handshake.reverse(); }
-  //if (reverseStr[4]==='1'){ handshake.reverse(); }
 
   console.log(`NUM: ${num}  STR: ${binaryStr}  HANDSHAKE: ${handshake} `)
    
