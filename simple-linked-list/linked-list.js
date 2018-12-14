17680a5b6a13b5889391aae80a4c0004dc23beec
David S. Jackson <deepbsd@yahoo.com>
2018-12-14 10:39:40 -0500

    adding secret handshake and simple-linked-list

--- simple-linked-list/linked-list.js
+++ simple-linked-list/linked-list.js
@@ -1,15 +1,43 @@
 // Implement a Singly Linked List
 class List {
-    constructor(){
-
+    constructor() {
+        this.length = 0;
+        this.head = null;
+    }
+    
+    _find(index) {
+        let node = this.head;
+        for (let i=0; i<index; i++) {
+            node = node.next;
+        }
+        return node;
     }
 
     pop(){
-
+        let prevNode = _find(this.length-1);
+        let node = prevNode.next;
+        prevNode.next = null;
+        this.length--;
+        return node;
     }
 
-    push(){
+    push(value) {
+        const newNode = {
+            value
+        };
 
+        // if we're pushing onto an empty list
+        if (this.length === 0){
+            this.head = newNode;
+            this.head.next = null;
+            this.length++;
+        }else{
+            // Find the node which we want to insert after
+            const node = this._find(this.length);
+            node.next = newNode;
+            newNode.next = node;
+            this.length++;
+        }
     }
 
     reverse(){
@@ -17,15 +45,25 @@ class List {
     }
 
     shift(){
-
+        // removing value from front of list
+        let node = this.head;
+        let this.head = node.next;
+        return node;
     }
 
     toArray(){
 
     }
 
-    unshift(){
+    unshift(value){
+        // Add value to beginning of list
+        let newNode = {
+            value
+        };
 
+        let node = this.head;
+        this.head = newNode;
+        this.head.next = node;
     }
 
 
