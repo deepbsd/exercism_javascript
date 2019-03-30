a9c442b8ae76c7034f08f5b90123c105b5c7e8c8
David S. Jackson <deepbsd@yahoo.com>
2019-03-29 23:40:53 -0400

    removed comment

--- resistor-colors/resistor-colors.js
+++ resistor-colors/resistor-colors.js
@@ -2,7 +2,6 @@
 export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
 
 export function value(colorArr){
-    //return COLORS.indexOf(colorArr[0]) + COLORS.indexOf(colorArr[1]);
     return parseInt(`${COLORS.indexOf(colorArr[0])}${COLORS.indexOf(colorArr[1])}`);
 }
 
