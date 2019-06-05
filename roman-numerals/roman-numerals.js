f3238d4fcc79d49fb2ecd0453140248eebc45e3b
David S. Jackson <deepbsd@yahoo.com>
2019-06-05 10:43:32 -0400

    completed roman-numerals

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,6 +1,5 @@
 export const toRoman = (number) => {
     
-    
     let romans = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'].reverse();
     let integers = [1,4,5,9,10,40,50,90,100,400,500,900,1000].reverse();
 
@@ -8,7 +7,6 @@ export const toRoman = (number) => {
 
     for (let n=0; n<integers.length; n++){
         let count = parseInt(number/integers[n], 10);
-        //result += romans[n] * count;
         result += romans[n].repeat(count);
         number -= integers[n] * count;
     }
