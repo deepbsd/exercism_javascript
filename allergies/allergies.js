db89d454df5e1cd7803e2fa9285ae7aeddb71604
David S. Jackson <deepbsd@yahoo.com>
2019-06-11 13:10:36 -0400

    updated allergies solution a bit

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -1,16 +1,10 @@
-//
-// This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Allergies {
   constructor(score) {
     this.score = score;
     this.allergies = ['eggs','peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats'];
-    this.allergylist = this.getList();
   }
 
-  getList() {
+  list() {
     let list = [];
     for (let i=0; i<this.allergies.length; i++){
         if (0 < (this.score & 1 << i)) list.push(this.allergies[i]);
@@ -18,12 +12,8 @@ export class Allergies {
     return list;
   }
 
-  list() {
-    return this.allergylist;
-  }
-
   allergicTo(allergen) {
-    return this.allergylist.includes(allergen);
+    return this.list().includes(allergen);
   }
 }
 
