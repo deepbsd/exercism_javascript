7fb20def6b45cba6572b6b1a6ebcdb8701da3505
David S. Jackson <deepbsd@yahoo.com>
2019-06-14 13:25:56 -0400

    started work on wordy

---
+++ wordy/wordy.js
@@ -0,0 +1,25 @@
+
+
+export function WordProblem(question) {
+    this.question = question;
+    this.ArgumentError = function(){ throw "Error!"; }
+    this.answer = this.add(1,1)
+}
+
+WordProblem.prototype.divide = function(first,second){
+    return first/second;
+}
+
+WordProblem.prototype.multiply = function(first,second){
+    return first * second;
+}
+
+WordProblem.prototype.minus = function(first,second){
+    return first - second;
+}
+
+WordProblem.prototype.add = function(first,second){
+    return first + second;
+}
+
+
