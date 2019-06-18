93d14882a7ae4967bfeabef7a14c51ecb612b895
David S. Jackson <deepbsd@yahoo.com>
2019-06-18 12:12:28 -0400

    okay, last iteration on wordy I think

--- wordy/wordy.js
+++ wordy/wordy.js
@@ -1,5 +1,3 @@
-
-
 export function WordProblem(question) {
     this.operators = ["plus", "minus", "multiplied", "divided"];
     this.question = question.replace(/ by/g,'').replace(/ is/g,'').replace(/What /g,'');
@@ -8,8 +6,8 @@ export function WordProblem(question) {
         if (question.indexOf('What') < 0) this.ArgumentError();
         let instructions = this.question.split(" ");
 
-        let func;
-        let args = []; let useArgs = []; let tempOperators = [];
+        let args = [];
+        let operatorArray = [];
 
         // iterate over all items, create arrays for operators and arguments
         instructions.map( elem => {
@@ -17,21 +15,21 @@ export function WordProblem(question) {
                 args.push(parseInt(elem,10));
             } 
             if (this.operators.includes(elem)){
-                if (elem === this.operators[0]) tempOperators.push(this.add); 
-                if (elem === this.operators[1]) tempOperators.push(this.minus);
-                if (elem === this.operators[2]) tempOperators.push(this.multiply);
-                if (elem === this.operators[3]) tempOperators.push(this.divide);
+                if (elem === this.operators[0]) operatorArray.push(this.add); 
+                if (elem === this.operators[1]) operatorArray.push(this.minus);
+                if (elem === this.operators[2]) operatorArray.push(this.multiply);
+                if (elem === this.operators[3]) operatorArray.push(this.divide);
             }
         });
         args = args.filter( elem => ! isNaN(elem));
         // All operators and arguments were put into arrays; if they're empty
         // return the calculated values, if not empty, keep shifting until they are.
-        func = tempOperators.shift();
-        useArgs = [ args.shift(), args.shift() ]
+        let func = operatorArray.shift();
+        let useArgs = [ args.shift(), args.shift() ]
         if (args.length === 0) return func(...useArgs); 
         else {
             let firstValue = func(...useArgs); 
-            let nextFunc = tempOperators.shift();
+            let nextFunc = operatorArray.shift();
             return nextFunc(firstValue, ...args);
         }
     } 
