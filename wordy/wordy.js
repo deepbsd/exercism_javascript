0623b9d79a35e4096bf0cab498036bcbf966786a
David S. Jackson <deepbsd@yahoo.com>
2019-06-18 11:41:24 -0400

    trying to tighten up wordy.js

--- wordy/wordy.js
+++ wordy/wordy.js
@@ -8,32 +8,31 @@ export function WordProblem(question) {
         if (question.indexOf('What') < 0) this.ArgumentError();
         let instructions = this.question.split(" ");
 
-        let operator, func, useFunc;
+        let func;
         let args = []; let useArgs = []; let tempOperators = [];
 
+        // iterate over all items, create arrays for operators and arguments
         instructions.map( elem => {
             if (typeof +elem === 'number') {
                 args.push(parseInt(elem,10));
             } 
             if (this.operators.includes(elem)){
-                operator = elem;
-                if (operator === this.operators[0]) func = this.add; 
-                if (operator === this.operators[1]) func = this.minus;
-                if (operator === this.operators[2]) func = this.multiply;
-                if (operator === this.operators[3]) func = this.divide;
-                tempOperators.push(func);
+                if (elem === this.operators[0]) tempOperators.push(this.add); 
+                if (elem === this.operators[1]) tempOperators.push(this.minus);
+                if (elem === this.operators[2]) tempOperators.push(this.multiply);
+                if (elem === this.operators[3]) tempOperators.push(this.divide);
             }
         });
         args = args.filter( elem => ! isNaN(elem));
-        useFunc = tempOperators.shift();
+        // All operators and arguments were put into arrays; if they're empty
+        // return the calculated values, if not empty, keep shifting until they are.
+        func = tempOperators.shift();
         useArgs = [ args.shift(), args.shift() ]
-        if (args.length === 0) return useFunc(...useArgs); 
+        if (args.length === 0) return func(...useArgs); 
         else {
-            let value = useFunc(...useArgs); 
+            let firstValue = func(...useArgs); 
             let nextFunc = tempOperators.shift();
-            let nextArgs = args;
-            let nextValue = nextFunc(value, ...nextArgs);
-            return nextValue;
+            return nextFunc(firstValue, ...args);
         }
     } 
 }
