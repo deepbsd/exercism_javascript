fc23fa8d5e3fc4e9308ef54d7c29035d5254aa49
David S. Jackson <deepbsd@yahoo.com>
2019-06-17 23:53:38 -0400

    still working on it

--- wordy/wordy.js
+++ wordy/wordy.js
@@ -9,39 +9,26 @@ export function WordProblem(question) {
         let instructions = this.question.split(" ");
         let args = [];
 
-
-
-        instructions.forEach( elem => {
+        instructions.map( elem => {
             if (elem.toLowerCase() === 'what' || elem.toLowerCase() === 'is') instructions.splice(instructions.indexOf(elem), 1);
             if (typeof +elem === 'number') {
-                args.push(parseInt(elem, 10));
+                args.push(+elem);
                 console.log("arguments: ",args)
-            }
+            } 
             if (this.arguments.includes(elem)){
-                
                 operation = elem;
-                console.log("operation: ",operation, " arguments: ", this.arguments[0], " equal? ",operation === this.arguments[0])
+                console.log("operation: ",operation, " args: ", args," arguments: ", this.arguments[0], " equal? ",operation === this.arguments[0])
                 if (operation === this.arguments[0]) {
-                    console.log("call self.add()");
-                    console.log("what is self? ", this)
-                    let self = this.super();
-                    return self.add(1,1);
+                    console.log("call this.add()");
+                    console.log("what is this? ", this)
+                    //return this.add(1,1);
                 }
                 else {
                     console.log("default")
                 }
-                    //case (operation === this.arguments[0]):
-                    //    console.log("call self.add()")
-                    //    //return self.add(1,1);
-                    //default:
-                    //    console.log("default")
-                    //    //return self.add(1,1);
             }
         })
-
-
-
-        //return this.add(1,1);
+        return this.add(1,1)
     } 
 }
 
@@ -61,4 +48,36 @@ WordProblem.prototype.add = function(first,second){
     return first + second;
 }
 
+        //instructions.forEach( elem => {
+        //    if (elem.toLowerCase() === 'what' || elem.toLowerCase() === 'is') instructions.splice(instructions.indexOf(elem), 1);
+        //    if (typeof +elem === 'number') {
+        //        args.push(parseInt(elem, 10));
+        //        console.log("arguments: ",args)
+        //    }
+        //    if (this.arguments.includes(elem)){
+        //        
+        //        operation = elem;
+        //        console.log("operation: ",operation, " arguments: ", this.arguments[0], " equal? ",operation === this.arguments[0])
+        //        if (operation === this.arguments[0]) {
+        //            console.log("call self.add()");
+        //            console.log("what is self? ", this)
+        //            let self = this.super();
+        //            return self.add(1,1);
+        //        }
+        //        else {
+        //            console.log("default")
+        //        }
+        //            //case (operation === this.arguments[0]):
+        //            //    console.log("call self.add()")
+        //            //    //return self.add(1,1);
+        //            //default:
+        //            //    console.log("default")
+        //            //    //return self.add(1,1);
+        //    }
+        //    throw "Bad input."
+        //})
+
+
+
+        //return this.add(1,1);
 
