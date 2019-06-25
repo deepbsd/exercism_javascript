811b1385fed4cd4049fe0f70d9c3a2f42bdfcd47
David S. Jackson <deepbsd@yahoo.com>
2019-06-24 21:47:51 -0400

    just starting work on clock

--- clock/clock.js
+++ clock/clock.js
@@ -1,14 +1,29 @@
-class Clock {
-  constructor(hrs, min){
-    this.hours = hrs;
-    this.minutes = min;
-  }
-  at(a, b){
-    var hrs = a/24;
-    var min = b/60;
-    return `${hrs.toFixed(2)}:${min.toFixed(2)}`;
+//
+// This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class at {
+  constructor(hrs,mins) {
+    this.mins = mins || 0;
+    this.hrs = hrs || 0;
   }
 
+  toString() {
+    let clock = new at(this.hrs,this.mins);
+    console.log("clock: ",this.hrs," : ",this.mins);
+    return `${clock.hours}:{clock.minutes}`;
+  }
+
+  plus() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  minus() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  equals() {
+    throw new Error("Remove this statement and implement this function");
+  }
 }
-
-module.exports = Clock;
