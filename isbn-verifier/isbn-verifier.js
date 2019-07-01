06cbe5b2a71236811e2c269d9a52b760e42eb975
David S. Jackson <deepbsd@yahoo.com>
2019-07-01 13:42:10 -0400

    cleaning up isbn-verifier

--- isbn-verifier/isbn-verifier.js
+++ isbn-verifier/isbn-verifier.js
@@ -1,15 +1,11 @@
 
 export class ISBN {
   constructor(string) {
-    this.string = string.replace(/[- ]/g, '');
-    this.numArr = this.string.split("");
+    this.numArr = string.split("").map(num => parseInt(num,10)).filter(num => Number.isInteger(num));
+    if (string.endsWith('X')) this.numArr[this.numArr.length] = 10;
   }
 
   isValid() {
-    if (this.numArr.length != 10 || this.string === '') return false;
-    if (this.numArr[9] === 'X') this.numArr[9] = 10;
-    let newNums = this.numArr.map( num => (isNaN(parseInt(num,10)) ? -1000 : parseInt(num,10)));
-        
-    return  newNums.reduce((total,curVal,idx,arr) => total += (arr.length-idx)*curVal, 0) % 11 === 0; 
+    return  this.numArr.reduce((total,curVal,idx,arr) => total += (arr.length-idx)*curVal, 0) % 11 === 0; 
   }
 }
