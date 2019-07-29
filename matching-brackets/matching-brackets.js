27746bf04bce59c01528b9c92bae5b58b1da47fb
David S. Jackson <deepbsd@yahoo.com>
2019-07-29 11:54:38 -0400

    finished passing tests for matching-brackets

--- matching-brackets/matching-brackets.js
+++ matching-brackets/matching-brackets.js
@@ -1,49 +1,22 @@
-// Basic solution: 
-// push each symbol onto 'toMatch' array in the order
-// encountered.  As we iterate through any closing symbols
-// (closed.includes(symbol)) must have the same index in closed 
-// as they have in the 'opened' array.  If we don't close
-// an opened bracket in the wrong order, return true.  Otherwise
-// return false.
 
-
-
-//export const matchingBrackets = (string) => {
-const matchingBrackets = (string) => {
-  let opened = ['{','[','('] ;
-  let closed = ['}',']',')'] ;
-  let toMatch = [];
+export const matchingBrackets = (string) => {
+  let matches = { '{':'}', '[':']', '(':')' };
+  let result;
   let inputArr = string.split("");
 
+  // does the closed symbol actually close the right opening symbol?
+  const isMatch = closedSym => (Object.values(matches).includes(closedSym) && 
+    Object.keys(matches).indexOf(inputArr[inputArr.indexOf(closedSym)-1]) === Object.values(matches).indexOf(closedSym));
 
-  // Last in must be first matched to be true, otherwise return false
-  inputArr.forEach( character => {  if (opened.includes(character)) toMatch.push(character) });
-
-
-  const isMatch = closedSym => (closed.includes(closedSym) && 
-        closed.indexOf(closedSym) === opened.indexOf(closedSym));
-
-
-
-  // Do the checking
+  // cycle through each character in the inputArr
   inputArr.forEach( character => { 
-    let result;
-    if (closed.includes(character)) {
-        isMatch(character) && 
-        inputArr[inputArr.indexOf(character)-1] === opened[opened.indexOf(character)] 
-        result = true;
+    if (Object.values(matches).includes(character)) {
+        if (isMatch(character)) result = true;
     } else {
         result = false;
     }
-    return result; 
   })
-
-
-
-
+  return result;
 };
 
 
-let testString = '{}';
-let result = matchingBrackets(testString);
-console.log(result)
