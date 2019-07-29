95069dd78884b98a104b424a32fea1ba0a5a3081
David S. Jackson <deepbsd@yahoo.com>
2019-07-29 12:21:48 -0400

    trying to clarify code

--- matching-brackets/matching-brackets.js
+++ matching-brackets/matching-brackets.js
@@ -1,14 +1,13 @@
-
 export const matchingBrackets = (string) => {
   let matches = { '{':'}', '[':']', '(':')' };
   let result;
   let inputArr = string.split("");
 
-  // does the closed symbol actually close the right opening symbol?
-  const isMatch = closedSym => (Object.values(matches).includes(closedSym) && 
-    Object.keys(matches).indexOf(inputArr[inputArr.indexOf(closedSym)-1]) === Object.values(matches).indexOf(closedSym));
+  const isMatch = closedSym => (
+    Object.values(matches).includes(closedSym) && 
+    Object.keys(matches).indexOf(inputArr[inputArr.indexOf(closedSym)-1]) === Object.values(matches).indexOf(closedSym)
+  );
 
-  // cycle through each character in the inputArr
   inputArr.forEach( character => { 
     if (Object.values(matches).includes(character)) {
         if (isMatch(character)) result = true;
