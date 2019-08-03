ffab03189c8ac9a64da10f2971958b97585325ae
David S. Jackson <deepbsd@yahoo.com>
2019-08-02 23:52:51 -0400

    working a bit on largest-series-product

--- largest-series-product/largest-series-product.js
+++ largest-series-product/largest-series-product.js
@@ -3,11 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const largestProduct = (numstring, length) => {
-    let numArr = numstring.split().map(val => parseInt(val,10),0);
-    let largestProduct = 0;
-
-    for (let i=0; i<numArr.length-length; i++){
-        let product = numArr.reduce( (total, curval, curIndex) => something good here... )
+const largestProduct = (numstring, length) => {
+//export const largestProduct = (numstring, length) => {
+    let numArr = numstring.split('').map(val => parseInt(val,10),0);
+    let start = 0; let end = length;
+    let slice = numArr.slice(start,end)
+    let product
+    let largestProduct = 0
+    while (slice !== numArr.slice(numArr.length-length,numArr.length)){
+        product = slice.reduce( (total, curval, curIndex) => curval * total, 1)
+        console.log(product)
+        if (product>largestProduct) largestProduct = product
+        slice = numArr.slice(start+length,end+length)
     }
+    console.log(largestProduct)
 };
+
+
+largestProduct('345123', 3)
