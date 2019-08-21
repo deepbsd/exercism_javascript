6025f7354bab759f26e25608cc79940eb737f0ad
David S. Jackson <deepbsd@yahoo.com>
2019-08-21 14:44:14 -0400

    7 tests passing now

--- say/say.js
+++ say/say.js
@@ -12,20 +12,22 @@ export class Say {
     const twenty_to_100 = (num) => {
         let s1 = num.slice(0,1)
         let s2 = num.slice(1,2)
-        return `${twenty_100[s2+'0']}-${up2_20[s1]}`
+        return `${twenty_100[s1+'0']}-${up2_20[s2]}`
       }
 
     const hundreds = (num) => {
         let s1 = num.slice(0,1)
         let s2 = num.slice(1,3)
 
-        if (s2 <= 20) s2 = twenty_100[s2]
-        else if (s2 > 20) s2 = twenty_to_100(s2)
+        if (s2 <= 20) s2 = up2_20[s2]
+        else if (s2 > 20) s2 = twenty_to_100(s2.toString())
+
+        console.log("S2: ",s2)
 
         if ([100, 200, 300, 400, 500, 600, 700, 800, 900].includes(num)){
             return `${up2_20[+s1]} hundred`
         } else {
-            return `${up2_20[+s1]} hundred and ${s2}`
+            return `${up2_20[s1]} hundred ${s2}`
         }
     }
 
@@ -34,6 +36,7 @@ export class Say {
     const callit = (num) => {
         if (num <= 20) return up2_20[num]
         if (num >= 20 && num <= 100) return twenty_to_100(num.toString()) 
+        if (num >= 100 && num < 1000) return hundreds(num.toString()) 
     }
 
 
