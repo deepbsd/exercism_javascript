5b51dac0a07d220c51f734a834f8f2f2c60f0e36
David S. Jackson <deepbsd@yahoo.com>
2019-08-27 13:02:02 -0400

    still working on twelve-days, passing 1 test

--- twelve-days/twelve-days.js
+++ twelve-days/twelve-days.js
@@ -1,37 +1,34 @@
-export const recite = (start,end) => {
+export const recite = (start,end=start) => {
 //const recite = (start,end) => {
 
     const days = ["first", "second", "third", "fourth","fifth", "sixth","seventh",
             "eighth", "ninth", "tenth", "eleventh", "twelfth"]
 
     const gifts = [
-            "and a Partridge in a Pear Tree",
-            "two Turtle Doves",
-            "three French Hens",
-            "four Calling Birds",
-            "five Gold Rings",
-            "six Geese-a-Laying",
-            "seven Swans-a-Swimming",
-            "eight Maids-a-Milking",
-            "nine Ladies Dancing",
-            "ten Lords-a-Leaping",
-            "eleven Pipers Piping",
-            "twelve Drummers Drumming",
+            " and a Partridge in a Pear Tree",
+            " two Turtle Doves",
+            " three French Hens",
+            " four Calling Birds",
+            " five Gold Rings",
+            " six Geese-a-Laying",
+            " seven Swans-a-Swimming",
+            " eight Maids-a-Milking",
+            " nine Ladies Dancing",
+            " ten Lords-a-Leaping",
+            " eleven Pipers Piping",
+            " twelve Drummers Drumming",
             ]
 
     let result = '', verse = ''
 
-    end = end ? end : end+1
 
-    // make end work in here...
-    // Also, get commas right...  those aren't working yet...
     for (let daynum of [...Array(start)].map( (_,i) => i)){
         let day = days[daynum]
-        let refrain = `On the ${day} day of Christmas my true love gave to me: `
-        if (day == "first") verse = refrain + "".concat(gifts.slice(0,daynum+1).reverse()).replace("and ","") + ".\n"
+        let refrain = `On the ${day} day of Christmas my true love gave to me:`
+        if (day == "first") verse = refrain + "".concat(gifts.slice(0,daynum+1).reverse()).replace("and ", "") + ".\n"
         else verse = refrain.concat(gifts.slice(0,daynum+1).reverse()) + "."
         result += verse
-        console.log("result:",result)
+        //console.log("result:",result)
     }
     return result
 
