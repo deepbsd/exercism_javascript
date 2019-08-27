192d62e5c8225c96e9af18c9307819b23aee9f50
David S. Jackson <deepbsd@yahoo.com>
2019-08-27 18:25:32 -0400

    still working on it, goofed something up

--- twelve-days/twelve-days.js
+++ twelve-days/twelve-days.js
@@ -19,20 +19,37 @@ export const recite = (start,end=start) => {
             " twelve Drummers Drumming",
             ]
 
-    let result = [], verse = '', num = start-1;
+    let result = [], verse = '', num, lineending;
 
+    if (end === start) num = start-1;
+    else num = end;
 
-    for (let daynum of [...Array(start)].map( (_,i) => i)){
-        let day = days[daynum]
-        let refrain = `On the ${day} day of Christmas my true love gave to me:`
-        if (day == "first") verse = refrain + "".concat(gifts.slice(0,daynum+1).reverse()).replace("and ", "") + ".\n"
-        else verse = refrain.concat(gifts.slice(0,daynum+1).reverse()) + ".\n"
-        result.push(verse)
+    function makeverse(versenum){
+        for (let daynum of [...Array(versenum)].map( (_,i) => i)){
+            let day = days[daynum]
+            let refrain = `On the ${day} day of Christmas my true love gave to me:`
+            if (day == "first") verse = refrain + "".concat(gifts.slice(0,daynum+1).reverse()).replace("and ", "") + ".\n"
+            else verse = refrain.concat(gifts.slice(0,daynum+1).reverse()) + ".\n"
+            result.push(verse)
+        }
+        return result[num]
     }
-    //console.log(result[num])
-    return result[num]
+    
+
+    let final = '';
+
+    console.log("start verse: ",start," end: ",end)
+
+    for (let versenum = start; versenum < end; versenum++) {
+        //if (versenum > 1) final += makeverse(versenum) + '\n'
+        //else final += makeverse(versenum) 
+        final += makeverse(versenum)
+    }
+
+    console.log("final: ",final)
+
+    return final
 
 };
 
 
-//console.log( recite(4) )
